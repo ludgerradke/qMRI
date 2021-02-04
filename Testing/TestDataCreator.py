707c3fafcaa2edb28b88aeadcfb09077a8dbf305
Ludger Radke <74981340+ludgerradke@users.noreply.github.com>
2021-02-04 21:22:26 +0100

    Bugfix of create Dicom template
    In the previous version the metadata was incorrectly added.

--- Testing/TestDataCreator.py
+++ Testing/TestDataCreator.py
@@ -5,7 +5,7 @@ import os
 from glob import glob
 import shutil
 
-from Testing.utils import create_test_dicom, create_dicom_folder
+from Testing.utils import create_test_dicom, create_dicom_test_folder, delete_dicom_test_folder
 
 
 class TestDicomCreator(ut.TestCase):
@@ -17,11 +17,12 @@ class TestDicomCreator(ut.TestCase):
         self.assertTrue(os.path.isfile(filename))
         os.remove(filename)
 
-    def test_dicom_folder_is_created(self):
+    def test_dicom_folder_is_created_and_deleted(self):
         number = 10
         folder = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
-        create_dicom_folder(folder, number)
+        create_dicom_test_folder(folder, number)
         self.assertTrue(os.path.exists(folder))
         self.assertTrue(len(glob(folder + '/*')), number)
-        shutil.rmtree(folder)
+        delete_dicom_test_folder(folder)
+        self.assertFalse(os.path.exists(folder))
 
